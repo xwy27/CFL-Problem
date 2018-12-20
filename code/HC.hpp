ddb3042f8afebcb065bd7ee2cea69f6d45294581
Weiyuan Xu <xuwy27@mail2.sysu.edu.cn>
2018-12-20 18:59:07 +0800

    Update HC.hpp

--- code/HC.hpp
+++ code/HC.hpp
@@ -29,6 +29,7 @@ public:
 
 Solution HC::twoOpt(Solution &s) {
     int size = s.allocate.size();
+    if (size == 0)  return Solution();
     int l = rand() % (size-1);
 	int r = rand() % (size-1);
     while(r == l)   r = rand() % size;
@@ -39,7 +40,8 @@ Solution HC::twoOpt(Solution &s) {
 }
 
 Solution HC::threeOpt(Solution &s) {
-	int size = s.allocate.size();
+    int size = s.allocate.size();
+    if (size == 0)  return Solution();
     int l = rand() % (size - 3);
     vector<int> temp(s.allocate.begin(), s.allocate.end());
     std::swap(temp[l], temp[l+1]);
@@ -67,30 +69,33 @@ vector<Solution> HC::neighborHood(Solution &s) {
 	cout << "Generate Neighbor" << endl;
 	#endif
     vector<Solution> neighbor;
-    neighbor.push_back(reverseOpt(s));
+    neighbor.push_back(this->reverseOpt(s));
     for (int i = 0; i < 1000; ++i) {
-        neighbor.push_back(twoOpt(s));
-        neighbor.push_back(threeOpt(s));
-        neighbor.push_back(randomOpt(s));
+        neighbor.push_back(this->twoOpt(s));
+        neighbor.push_back(this->threeOpt(s));
+        neighbor.push_back(this->randomOpt(s));
     }
     return neighbor;
 }
 
 Solution HC::nextSolution(Solution &s, vector<int> &open, vector<vector<int> > &allocation,
     vector<int> &capicity, vector<int> &demand) {
-	#if DEBUG
-	cout << "next solution" << endl;
-	#endif
-    int cost = s.getCost(open, allocation, capicity, demand), temp = 0;
-    Solution ans;
-    int index = 0;
-    for (auto &i : this->neighborHood(s)) {
-		temp = i.getCost(open, allocation, capicity, demand);		
-        if (temp < cost) {
-            ans = i;
-            cost = temp;
-	    }	
-   }
+    #if DEBUG
+    cout << "next solution" << endl;
+    #endif
+    int cost = s.getCost(open, allocation, capicity, demand);
+    int temp = 0;
+    Solution ans = s;
+    vector<Solution> t = this->neighborHood(s);
+    int i = 0;
+    for (i = 0; i < t.size(); ++i) {
+      if (t[i].allocate.size() == 0) continue;
+      temp = t[i].getCost(open, allocation, capicity, demand);
+      if (temp < cost && temp != 0) {
+        ans = t[i];
+        cost = temp;
+      }
+    }
     return ans;
 }
 
